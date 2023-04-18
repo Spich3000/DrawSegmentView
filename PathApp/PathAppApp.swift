2ad17e2b1903086a4d7dbcda0bd34e0523c215f6
Spich <spichakovda@gmail.com>
2023-04-18 23:04:59 +0300

    Initial Commit

---
+++ PathApp/PathAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathAppApp.swift
+//  PathApp
+//
+//  Created by Дмитрий Спичаков on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PathAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
